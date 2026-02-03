dbad5c2b013ebd2e28d1afd0c1741d91f68da2c7
Faro19748 <faro19748@gmail.com>
2026-02-03 16:09:36 +0700

    up

--- Lab/app/src/main/java/com/example/lab/MainActivity.kt
+++ Lab/app/src/main/java/com/example/lab/MainActivity.kt
@@ -1,47 +1,203 @@
 package com.example.lab
 
+
+
 import android.os.Bundle
+
 import androidx.activity.ComponentActivity
+
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+
+import androidx.compose.foundation.Image
+
+import androidx.compose.foundation.background
+
+import androidx.compose.foundation.layout.*
+
+import androidx.compose.material3.*
+
+import androidx.compose.runtime.getValue
+
+import androidx.compose.runtime.mutableStateOf
+
+import androidx.compose.runtime.remember
+
+import androidx.compose.runtime.setValue
+
+import androidx.compose.ui.Alignment
+
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.lab.ui.theme.LabTheme
+
+import androidx.compose.ui.graphics.Color
+
+import androidx.compose.ui.res.painterResource
+
+import androidx.compose.ui.unit.dp
+
+import androidx.compose.ui.unit.sp
+
+
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+//enableEdgeToEdge()
+
         setContent {
-            LabTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+
+            Column (modifier = Modifier.fillMaxSize().background(color = Color.Gray).padding(32.dp)){
+
+// hp
+
+                Box(modifier = Modifier
+
+                    .height(32.dp)
+
+                    .background(color = Color.White)
+
+                    .fillMaxWidth()
+
+
+
+
+
+                ){
+
+                    Text(
+
+                        text = "HP",
+
+                        modifier = Modifier
+
+                            .align(alignment = Alignment.CenterStart)
+
+                            .fillMaxWidth(fraction = 0.10f)
+
+                            .background(color = Color.Red)
+
+                            .padding(8.dp)
+
                     )
+
                 }
+
+// image
+
+                Image(
+
+                    painter = painterResource(id = R.drawable.peter),
+
+                    contentDescription = "Porfile",
+
+                    modifier = Modifier
+
+                        .size(size = 400.dp)
+
+                        .align(Alignment.CenterHorizontally)
+
+                        .padding(top = 32.dp)
+
+
+
+
+
+
+
+                )
+
+                var STR by remember { mutableStateOf(value = 5) }
+                var AGI by remember { mutableStateOf(value = 3) }
+                var DEF by remember { mutableStateOf(value = 7) }
+                var INT by remember { mutableStateOf(value = -2) }
+                var isClicked by remember { mutableStateOf(false) }
+
+
+
+
+
+//status
+
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Absolute.SpaceAround
+
+                ){
+
+                    Column {
+                        Button(onClick = {
+                            STR = STR + 1
+
+                        }) {
+                            Text(text = "+", fontSize = 32.sp)
+
+                        }
+                        Text(text = "STR", fontSize = 32.sp)
+                        Text(text = STR.toString(), fontSize = 32.sp)
+
+
+
+                    }
+
+                    Column {
+                        Button(onClick = {
+                            AGI = AGI + 1
+
+                        }) {
+                            Text(text = "+", fontSize = 32.sp)
+
+                        }
+                        Text(text = "AGI", fontSize = 32.sp)
+                        Text(text = AGI.toString(), fontSize = 32.sp)
+
+
+
+                    }
+
+                    Column {
+
+                        Button(onClick = {
+                            DEF = DEF+ 1
+                        }) {
+                            Text(text = "+", fontSize = 32.sp)
+                        }
+                        Text(text = "DEF", fontSize = 32.sp)
+
+                        Text(text = DEF.toString(), fontSize = 32.sp)
+
+
+
+                    }
+
+                    Column {
+                        Button(onClick = {
+                            INT = INT + 1
+                            isClicked = true // เปลี่ยนสถานะเมื่อกด
+                        }) {
+                            Text(text = "+", fontSize = 32.sp)
+                        }
+                        Text(text = "INT", fontSize = 32.sp)
+
+                        // เช็คว่าถ้ากดแล้วให้แสดง HI ถ้ายังไม่กดให้แสดงตัวเลข
+                        if (isClicked) {
+                            Text(text = "HI", fontSize = 32.sp)
+                        } else {
+                            Text(text = INT.toString(), fontSize = 32.sp)
+                        }
+                    }
+
+                }
+
+
+
+
+
             }
+
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LabTheme {
-        Greeting("Android")
     }
+
 }
