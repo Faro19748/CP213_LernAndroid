4d4c017e309631c88a1e2366f6d8a80b8562884c
Faro19748 <faro19748@gmail.com>
2026-02-03 14:24:59 +0700

    up

---
+++ Lab/app/src/main/java/com/example/lab/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.lab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.lab.R
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContent {
+            Column (modifier = Modifier.fillMaxSize().background(color = Color.Gray).padding(32.dp)){
+                // hp
+                Box(modifier = Modifier
+                    .height(32.dp)
+                    .background(color = Color.White)
+                    .fillMaxWidth()
+
+
+                ){
+                    Text(
+                        text = "HP",
+                        modifier = Modifier
+                            .align(alignment = Alignment.CenterStart)
+                            .fillMaxWidth(fraction = 0.10f)
+                            .background(color =  Color.Red)
+                            .padding(8.dp)
+                    )
+                }
+                // image
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "Porfile"
+
+
+
+                )
+                //status
+            }
+        }
+    }
+}
